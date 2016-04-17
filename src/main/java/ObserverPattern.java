d891bc1f52fd0a1c229386aabecfcc0a0599c420
Chrissy Soulakian <csoulakian@luc.edu>
2016-04-17 13:52:13 -0500

    created initial project

---
+++ src/main/java/ObserverPattern.java
@@ -0,0 +1,11 @@
+/**
+ * Chrissy Soulakian
+ * ObserverPattern Project
+ * Created on 4/17/2016
+ */
+public class ObserverPattern {
+
+    public static void main(String[] args) {
+        
+    }
+}
